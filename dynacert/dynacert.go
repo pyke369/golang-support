cd1ed731523d8e5d99903bde7f7848d73b723bdf
Pierre-Yves Kerembellec <py.kerembellec@gmail.com>
2020-03-21 19:47:31 +0100

    use intermediate TLS configuration by default
    see https://ssl-config.mozilla.org/#server=golang&version=1.13.6&config=intermediate&guideline=5.4

--- dynacert/dynacert.go
+++ dynacert/dynacert.go
@@ -82,26 +82,21 @@ func (this *DYNACERT) GetCertificate(client *tls.ClientHelloInfo) (cert *tls.Cer
 	return nil, errors.New(`dynacert: no matching certificate`)
 }
 
-func ModernTLSConfig(input func(*tls.ClientHelloInfo) (*tls.Certificate, error)) (output *tls.Config) {
+func IntermediateTLSConfig(input func(*tls.ClientHelloInfo) (*tls.Certificate, error)) (output *tls.Config) {
 	output = &tls.Config{
-		PreferServerCipherSuites: true,
-		CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.X25519},
-		MinVersion:               tls.VersionTLS11,
+		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			// TLS 1.3
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			// TLS 1.2
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 		},
 	}
 	if input != nil {
