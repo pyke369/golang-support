e039aec79bd85c9c276339c174ceb0d238d55f39
Pierre-Yves Kerembellec <py.kerembellec@gmail.com>
2021-10-22 17:20:16 +0200

    add times/dates-based ACL support

--- acl/acl.go
+++ acl/acl.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/pyke369/golang-support/rcache"
 	"github.com/pyke369/golang-support/uconfig"
 )
 
@@ -175,27 +176,30 @@ func Password(input string, values []string) bool {
 	}
 	for _, value := range values {
 		check := strings.TrimSpace(value)
-		if login != "" {
-			if parts := strings.Split(value, ":"); len(parts) < 2 || login != strings.TrimSpace(parts[0]) {
-				continue
-			} else {
-				check = strings.TrimSpace(parts[1])
-			}
-		}
-		if parts := strings.Split(check, "$"); len(parts) >= 4 && parts[0] == "" && parts[1] == "6" && parts[2] != "" && parts[3] != "" {
-			rounds, salt := 5000, parts[2]
-			if len(parts) > 4 && strings.HasPrefix(parts[2], "rounds=") {
-				rounds, _ = strconv.Atoi(parts[2][7:])
-				salt = parts[3]
-			}
-			if Crypt512(password, salt, rounds) == check {
+		if len(check) > 1 && check[0] == '@' {
+			if PasswordFile(input, strings.TrimSpace(check[1:])) {
 				return true
 			}
-		} else if password == check {
-			return true
-		}
-		if login != "" {
-			break
+		} else {
+			if login != "" {
+				if parts := strings.Split(value, ":"); len(parts) < 2 || login != strings.TrimSpace(parts[0]) {
+					continue
+				} else {
+					check = strings.TrimSpace(parts[1])
+				}
+			}
+			if parts := strings.Split(check, "$"); len(parts) >= 4 && parts[0] == "" && parts[1] == "6" && parts[2] != "" && parts[3] != "" {
+				rounds, salt := 5000, parts[2]
+				if len(parts) > 4 && strings.HasPrefix(parts[2], "rounds=") {
+					rounds, _ = strconv.Atoi(parts[2][7:])
+					salt = parts[3]
+				}
+				if Crypt512(password, salt, rounds) == check {
+					return true
+				}
+			} else if password == check {
+				return true
+			}
 		}
 	}
 	return false
@@ -214,4 +218,63 @@ func PasswordFile(input, path string) bool {
 		}
 	}
 	return Password(input, lines)
+}
+
+type RANGE struct {
+	dates [2]time.Time
+	days  [2]int
+	times [2]int
+}
+
+func Ranges(input time.Time, values []string) (match bool) {
+	if len(values) > 0 {
+		ranges, matcher1, matcher2, matcher3, days := []RANGE{},
+			rcache.Get(`^(\d{4}-\d{2}-\d{2})?-(\d{4}-\d{2}-\d{2})?$`),
+			rcache.Get(`^(mon|tue|wed|thu|fri|sat|sun)?-(mon|tue|wed|thu|fri|sat|sun)?$`),
+			rcache.Get(`^(?:(\d{2}):(\d{2})(?::(\d{2}))?)?-(?:(\d{2}):(\d{2})(?::(\d{2}))?)?$`),
+			map[string]int{"mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5, "sat": 6, "sun": 7}
+		for _, path := range values {
+			entry := RANGE{}
+			for _, value := range strings.Split(path, " ") {
+				if captures := matcher1.FindStringSubmatch(value); len(captures) == 3 {
+					if value, err := time.Parse("2006-01-02", captures[1]); err == nil {
+						entry.dates[0] = value
+					}
+					if value, err := time.Parse("2006-01-02", captures[2]); err == nil {
+						entry.dates[1] = value.Add(86399 * time.Second)
+					}
+				} else if captures := matcher2.FindStringSubmatch(strings.ToLower(value)); len(captures) == 3 {
+					entry.days[0], entry.days[1] = days[captures[1]], days[captures[2]]
+				} else if captures := matcher3.FindStringSubmatch(value); len(captures) == 7 {
+					hour, _ := strconv.ParseInt(captures[1], 10, 64)
+					minute, _ := strconv.ParseInt(captures[2], 10, 64)
+					second, _ := strconv.ParseInt(captures[3], 10, 64)
+					entry.times[0] = int(hour)*3600 + int(minute)*60 + int(second)
+					hour, _ = strconv.ParseInt(captures[4], 10, 64)
+					minute, _ = strconv.ParseInt(captures[5], 10, 64)
+					second, _ = strconv.ParseInt(captures[6], 10, 64)
+					entry.times[1] = int(hour)*3600 + int(minute)*60 + int(second)
+				}
+			}
+			ranges = append(ranges, entry)
+		}
+		now := input.UTC()
+		day, stamp := int(now.Weekday()), now.Hour()*3600+now.Minute()*60+now.Second()
+		if day == 0 {
+			day = 7
+		}
+		for _, entry := range ranges {
+			if (!entry.dates[0].IsZero() && now.Sub(entry.dates[0]) < 0) || (!entry.dates[1].IsZero() && now.Sub(entry.dates[1]) > 0) ||
+				(entry.days[0] != 0 && day < entry.days[0]) || (entry.days[1] != 0 && day > entry.days[1]) ||
+				(entry.times[0] != 0 && stamp < entry.times[0]) || (entry.times[1] != 0 && stamp > entry.times[1]) {
+				continue
+			}
+			return true
+		}
+		return false
+	}
+	return true
+}
+func RangesConfig(input time.Time, config *uconfig.UConfig, path string) bool {
+	return Ranges(input, config.GetStrings(path))
 }
