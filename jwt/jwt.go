6620426d266d6ac5552ce939ea1e390992af9909
Pierre-Yves Kerembellec <py.kerembellec@gmail.com>
2023-01-02 18:00:47 +0100

    support multiple algorithms and secrets in jwt

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,17 +1,49 @@
 package jwt
 
 import (
+	"crypto"
+	"crypto/ecdsa"
 	"crypto/hmac"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
+	"math/big"
 	"strings"
 	"time"
 )
 
-func Encode(claims map[string]any, expire time.Time, secret string) (token string, err error) {
-	token = base64.RawURLEncoding.EncodeToString([]byte(`{"typ":"JWT","alg":"HS256"}`)) + "."
+func Encode(claims map[string]any, expire time.Time, secret string, kid ...string) (token string, err error) {
+	var block *pem.Block
+
+	alg := "HS256"
+	if secret == "" {
+		alg = "none"
+	} else if len(kid) != 0 {
+		rest := []byte(secret)
+		for {
+			if block, rest = pem.Decode(rest); block == nil {
+				return "", fmt.Errorf("jwt: invalid private key")
+			}
+			if block.Type == "RSA PRIVATE KEY" || block.Type == "EC PRIVATE KEY" {
+				if block.Type == "RSA PRIVATE KEY" {
+					alg = "RS256"
+				} else {
+					alg = "ES256"
+				}
+				break
+			}
+		}
+	}
+	token = `{"typ":"JWT","alg":"` + alg + `"`
+	if len(kid) != 0 {
+		token += `,"kid":"` + kid[0] + `"`
+	}
+	token = base64.RawURLEncoding.EncodeToString([]byte(token+`}`)) + "."
 	if claims == nil {
 		claims = map[string]any{}
 	}
@@ -23,13 +55,46 @@ func Encode(claims map[string]any, expire time.Time, secret string) (token strin
 		return "", err
 	}
 	token += base64.RawURLEncoding.EncodeToString(marshaled)
-	mac := hmac.New(sha256.New, []byte(secret))
-	mac.Write([]byte(token))
-	token += "." + base64.RawURLEncoding.EncodeToString(mac.Sum(nil))
+	if alg == "none" {
+		token += "."
+	} else if alg == "HS256" {
+		signature := hmac.New(sha256.New, []byte(secret))
+		signature.Write([]byte(token))
+		token += "." + base64.RawURLEncoding.EncodeToString(signature.Sum(nil))
+	} else if alg == "RS256" {
+		key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return "", fmt.Errorf("jwt: %v", err)
+		}
+		sum := sha256.Sum256([]byte(token))
+		signature, err := rsa.SignPKCS1v15(rand.Reader, key, crypto.SHA256, sum[:])
+		if err != nil {
+			return "", fmt.Errorf("jwt: %v", err)
+		}
+		token += "." + base64.RawURLEncoding.EncodeToString(signature)
+	} else if alg == "ES256" {
+		var signature [64]byte
+
+		key, err := x509.ParseECPrivateKey(block.Bytes)
+		if err != nil {
+			return "", fmt.Errorf("jwt: %v", err)
+		}
+		if key.Curve.Params().BitSize != 256 {
+			return "", fmt.Errorf("jwt: invalid elliptic curve size %d", key.Curve.Params().BitSize)
+		}
+		sum := sha256.Sum256([]byte(token))
+		r, s, err := ecdsa.Sign(rand.Reader, key, sum[:])
+		if err != nil {
+			return "", fmt.Errorf("jwt: %v", err)
+		}
+		r.FillBytes(signature[:32])
+		s.FillBytes(signature[32:])
+		token += "." + base64.RawURLEncoding.EncodeToString(signature[:])
+	}
 	return
 }
 
-func Decode(token, secret string) (claims map[string]any, err error) {
+func Decode(token string, secrets []string) (claims map[string]any, err error) {
 	parts := strings.Split(token, ".")
 	if len(parts) != 3 {
 		return nil, fmt.Errorf("jwt: invalid token format")
@@ -42,17 +107,53 @@ func Decode(token, secret string) (claims map[string]any, err error) {
 	if err := json.Unmarshal(decoded, &header); err != nil {
 		return nil, err
 	}
-	if header["typ"] != "JWT" || header["alg"] != "HS256" {
+	if (header["typ"] != "" && header["typ"] != "JWT") || (header["alg"] != "none" && header["alg"] != "HS256" && header["alg"] != "RS256" && header["alg"] != "ES256") {
 		return nil, fmt.Errorf("jwt: unsupported token format")
 	}
 	decoded, err = base64.RawURLEncoding.DecodeString(parts[2])
 	if err != nil {
 		return nil, err
 	}
-	if secret != "" {
-		mac := hmac.New(sha256.New, []byte(secret))
-		mac.Write([]byte(parts[0] + "." + parts[1]))
-		if !hmac.Equal(mac.Sum(nil), decoded) {
+	if len(secrets) > 0 && header["alg"] != "none" {
+		pass := false
+		for _, secret := range secrets {
+			if header["alg"] == "HS256" {
+				signature := hmac.New(sha256.New, []byte(secret))
+				signature.Write([]byte(parts[0] + "." + parts[1]))
+				if hmac.Equal(signature.Sum(nil), decoded) {
+					pass = true
+				}
+			} else {
+				var block *pem.Block
+
+				rest := []byte(secret)
+				for {
+					if block, rest = pem.Decode(rest); block == nil {
+						break
+					}
+					if block.Type == "PUBLIC KEY" {
+						if key, err := x509.ParsePKIXPublicKey(block.Bytes); err == nil {
+							switch key := key.(type) {
+							case *rsa.PublicKey:
+								if header["alg"] == "RS256" && len(decoded) == 256 {
+									sum := sha256.Sum256([]byte(parts[0] + "." + parts[1]))
+									pass = rsa.VerifyPKCS1v15(key, crypto.SHA256, sum[:], decoded) == nil
+								}
+							case *ecdsa.PublicKey:
+								if header["alg"] == "ES256" && len(decoded) == 64 {
+									sum := sha256.Sum256([]byte(parts[0] + "." + parts[1]))
+									pass = ecdsa.Verify(key, sum[:], big.NewInt(0).SetBytes(decoded[:32]), big.NewInt(0).SetBytes(decoded[32:]))
+								}
+							}
+						}
+					}
+				}
+			}
+			if pass == true {
+				break
+			}
+		}
+		if !pass {
 			return nil, fmt.Errorf("jwt: invalid signature")
 		}
 	}
